83c4d994d1a88b1e3d88dc105663e11f5910a796
zfg663 <zfg663@alumni.ku.dk>
2020-07-07 22:38:06 +0200

    Running 1-link simulation with beta=0.7

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int FoodWeb::prevIteration = 0;
 int FoodWeb::prevExtinct = 0;
 // default behavior of last iteration
 
-double addAttemptMAX = 4;
+double addAttemptMAX = 10000;
 // max number of addition attempts
 
 
@@ -28,17 +28,17 @@ int main()
 	srand(2);
 
 //	FILES
-	ofstream stabEigen("data/eig_stab.txt");		// eigenvalues of linearly stable food web
-	ofstream ustabEigen("data/eig_unstab.txt");		// eigenvalues of linearly unstable food web
+	ofstream stabEigen("data/1-link/eig_stab.txt");		// eigenvalues of linearly stable food web
+	ofstream ustabEigen("data/1-link/eig_unstab.txt");		// eigenvalues of linearly unstable food web
 	// data is saved as: iteration - number of Species - real - imaginary
 
-	ofstream webData("data/stab_data.txt");				// behavior of food web
+	ofstream webData("data/1-link/stab_data.txt");				// behavior of food web
 	// data is saved as:
 	// iteration - number of Species - feasibility - stability - convergence
 	// 1 - steady, 2 - damped oscillations to steady, 3 - periodic, 4 - chaotic, 88 - unknown
 
-	ofstream sFile("data/species_param.txt");			// Species parameters
-	ofstream pFile("data/producer_param.txt");			// Producer parameters
+	ofstream sFile("data/1-link/species_param.txt");			// Species parameters
+	ofstream pFile("data/1-link/producer_param.txt");			// Producer parameters
 	// data is saved as:
 	// iteration - l - S(0) - k - a - consumer - eta
 	// iteration - l - S(0) - a - consumer/resource - eta - beta*eta
