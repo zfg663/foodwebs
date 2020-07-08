730ccfcb8f6f3da0b1bab3339e663319c1a2d933
zfg663 <zfg663@alumni.ku.dk>
2020-07-08 15:06:47 +0200

    Added 'spectra'

--- eigenvalue_code/random_matrix.cpp
+++ eigenvalue_code/random_matrix.cpp
@@ -1,9 +1,10 @@
+// Script to compute eigenvalues of random matrices
+// 23/5-2020: Used random seed 27 to run simulations for N = 2-12
+
+#include "/conv1/zfg663/foodwebs/evolution_code/distributions.cpp"
 #include <iostream>
 #include <fstream>
-
-#include <algorithm>
-#include <cstdlib>
-#include <cmath>
+#include <random>
 #include <Eigen/Dense>
 #include <Eigen/Eigenvalues>
 
@@ -12,14 +13,6 @@ using namespace std;
 
 
 // Defining functions used in script
-double randomDouble(double min, double max) {
-	return min + (max - min)*((double)rand() / (double)RAND_MAX);
-}
-
-int randomInt(int min, int max) {
-	return min + rand() % (max - min + 1);
-}
-
 void printMatrix(MatrixXd M, int N) {
 	for(int i=0; i<N; i++) {
 		for(int j=0; j<N; j++) { cout << "C(" << i << "," << j << "): " << M(i, j) << " "; }
@@ -27,41 +20,57 @@ void printMatrix(MatrixXd M, int N) {
 	}
 }
 
-
 int main() {
 
-ofstream file("eigenvalues.txt");
-
+// setting parameters, etc.
 srand(27);
 
-int min = 0;
-int max = 1;
-int N = 4;			// number of species
+double prob = 0.5;		// occupation probability
+int mu = 0;			// mean of gaussian
+int sig = 1;			// std of gaussian
+int Nrep = 1e4;			// number of community matrices in spectrum
+int N = 2;			// number of species
+int d = 1;			// diagonal element
 
-MatrixXd C(N, N);		// community matrix
+ofstream file("/conv1/zfg663/foodwebs/data/spectra/May_N12.txt");
 
 
-// filling community matrix with random numbers
-for(int i=0; i<N; i++) {
-	for(int j=0; j<N; j++) {
-		C(i,j) = randomDouble(min, max);
+for(int rep=0; rep<Nrep; rep++) {
+	MatrixXd C(N, N);		// community matrix
+	VectorXcd E(N);			// Eigen values
+	
+	random_device rd;
+	mt19937 e2(rd());
+	normal_distribution<> dist(mu, sig);
+
+	// filling community matrix with random numbers
+	for(int i=0; i<N; i++) {
+		for(int j=0; j<N; j++) {
+		 	double u = randomDouble(0,1);
+			if (u < prob) {
+				C(i,j) = dist(e2);
+			}
+		}
 	}
+	
+	// filling diagonal
+	for (int i=0; i<N; i++) {
+		C(i,i) = -d;
+	}
+
+	//printMatrix(C,N);
+
+	// computing eigenvalues
+	EigenSolver<MatrixXd> es(C);
+	E = es.eigenvalues();
+
+	// saving
+	for (int i=0; i<N; i++) {
+		file << E(i).real() << " " << E(i).imag() << " ";
+	}
+	file << endl;
 }
 
-// printing matrix
-printMatrix(C, N);
-
-// computing eigenvalues
-EigenSolver<MatrixXd> es(C);
-VectorXcd eigenvalues(N);
-eigenvalues = es.eigenvalues();
-
-
-// saving
-for (int i=0; i<N; i++) {
-	file << eigenvalues(i).real() << " " << eigenvalues(i).imag() << " ";
-}
-file << endl;
-
+file.close();
 return 0;
 }
